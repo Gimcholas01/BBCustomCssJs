d3bd0ecd8ec53f0d9b91494e375a4ffd54105d10
Lim <112832949+Gimcholas01@users.noreply.github.com>
2026-03-08 20:59:29 +0800

    init

---
+++ BBCustomCssJs.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * Plugin Name: BBCustomCssJs
+ * Author: Gimcholas@BB
+ * Description: Injects CSS and JS into every page
+ */
+
+function getCustomCodeDir()
+{
+    $upload_dir = wp_upload_dir();
+    $customCodeDir = $upload_dir['basedir'] . '/' . get_plugin_data(__FILE__)['Name'];
+    return $customCodeDir;
+}
+
+function getCustomCodeUrl()
+{
+    $upload_dir = wp_upload_dir();
+    $customCodeDir = $upload_dir['baseurl'] . '/' . get_plugin_data(__FILE__)['Name'];
+    return $customCodeDir;
+}
+
+function BBCustomCssJs_activate()
+{
+    wp_mkdir_p(getCustomCodeDir());
+}
+
+function InjectStyle($url)
+{
+    return '<link rel="stylesheet" href="' . $url . '">';
+}
+
+function InjectScript($url)
+{
+    return '<script src="' . $url . '"></script>';
+}
+
+function InjectCustomCssCode()
+{
+    if (is_admin()) {
+        return;
+    }
+
+    $customCodeUrl = getCustomCodeUrl();
+
+    foreach (glob(getCustomCodeDir() . '/*.css') as $filename) {
+        echo InjectStyle($customCodeUrl . '/' . basename($filename));
+    }
+}
+
+function InjectCustomJsCode()
+{
+    if (is_admin()) {
+        return;
+    }
+
+    $customCodeUrl = getCustomCodeUrl();
+
+    foreach (glob(getCustomCodeDir() . '/*.js') as $filename) {
+        echo InjectScript($customCodeUrl . '/' . basename($filename));
+    }
+}
+
+register_activation_hook(__FILE__, 'BBCustomCssJs_activate');
+add_action('wp_head', 'InjectCustomCssCode', 999);
+add_action('wp_print_footer_scripts', 'InjectCustomJsCode');
